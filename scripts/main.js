172bf6b32cad166e711bc20da431b27bf7ed55dc
Maksim Drachov <maksim.nesterenko@unikie.com>
2022-05-29 14:52:07 +0300

    new

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,26 @@
 
 // Personalized welcome message code
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Welcome to e-estonia, ' + myName;
-  }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Welcome to e-estonia, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
+//let myButton = document.querySelector('button');
+//let myHeading = document.querySelector('h1');
+//
+//function setUserName() {
+//  let myName = prompt('Please enter your name.');
+//  if(!myName) {
+//    setUserName();
+//  } else {
+//    localStorage.setItem('name', myName);
+//    myHeading.innerHTML = 'Welcome to hackernews.ee, ' + myName;
+//  }
+//}
+//
+//if(!localStorage.getItem('name')) {
+//  setUserName();
+//} else {
+//  let storedName = localStorage.getItem('name');
+//  myHeading.innerHTML = 'Welcome to hackernews.ee, ' + storedName;
+//}
+//
+//myButton.onclick = function() {
+//  setUserName();
+//}
